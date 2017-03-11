b64c20c1b4cf4c87289f5e27789eb4463024894d
Francine Biazin do Nascimento <francine.biazin@gmail.com>
2017-03-11 16:17:47 +0000

    Initial commit

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,38 @@
 from datetime import datetime
 
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = None
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
-
-
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def set_user(self, user):
+        self.user = user
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
+
+    def __str__(self):
+        return '@{user}: "{text}"\n\t{timestamp}'.format(user=self.user, text=self.text, timestamp=self.timestamp.strftime("%A, %b %d, %Y"))
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text, timestamp)
+        self.image_url = image_url
+
+    def __str__(self):
+        return '@{user}: "{text}"\n\t{image_url}\n\t{timestamp}'.format(user=self.user, text=self.text, image_url=self.image_url, timestamp=self.timestamp.strftime("%A, %b %d, %Y"))
+
+
+
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return '@{user} Checked In: "{text}"\n\t{latitude}, {longitude}\n\t{timestamp}'.format(user=self.user.first_name, text=self.text, latitude=self.latitude, longitude=self.longitude, timestamp=self.timestamp.strftime("%A, %b %d, %Y"))
